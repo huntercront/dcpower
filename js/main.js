449692d57fa775eb27ba144279392bee4a194683
huntercront <38252870+huntercront@users.noreply.github.com>
2018-10-17 17:07:59 +0300

    menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("m_menu");
+var btnm = document.getElementById("m_memu_text");
+	btn.onclick= function() {
+		btnm.classList.toggle("main-navigation-container_active");
+					btn.classList.toggle("menu-btn_active");
+			}
