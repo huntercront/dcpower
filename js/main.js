24f958d1897bfac43c2a66d6f15c76879bd9c1a0
huntercront <38252870+huntercront@users.noreply.github.com>
2018-11-07 17:57:53 +0300

    start create site

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ var btnm = document.getElementById("m_memu_text");
 		btnm.classList.toggle("main-navigation-container_active");
 					btn.classList.toggle("menu-btn_active");
 			}
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
